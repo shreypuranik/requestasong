9bd0f96832dcbe45f8dd081114078c71c3bd5557
shreypuranik <code@shreypuranik.co.uk>
2017-03-10 23:20:51 +0000

    Bringing in basic Bootstrap integration

--- index2.php
+++ index2.php
@@ -2,13 +2,53 @@
 include_once("config.php");
 include_once("classes/class.Search.php");
 ?>
+
+<html>
+<title>Request A Song</title>
+<head>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <script src="jquery-1.11.2.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<style>
+    .footerbox{
+        background: #d25143;
+        color: white;
+    }
 
-<h2>Request a song: A simple PHP & MYSQL based music request system</h2>
+    .bs_input{
+        width: 50%;
+    }
 
+    .portal-start {
+        margin-top: 10px;
+    }
+
+    #radioForm ul {
+        list-style-type: none;
+    }
+</style>
+</head>
+<body>
+<div class="row footerbox">
+    <div class="col-sm-10 ">
+        <h2>Request a song</h2>
+    </div>
+    <div class="col-sm-2">
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="col-sm-2 ">
+        </div>
+
+    <div class="col-sm-9 portal-start">
+        <p>Welcome to the Request A Song portal. Search for the name of the artist or group you want to request a song from. </p>
 <form name="radioForm" id="radioForm">
     <ul>
-        <li>What do you want to listen to? : <input type="text" name="searchParam" id="searchParam"></li>
+        <li>What do you want to listen to? : <input type="text" name="searchParam" id="searchParam" class="bs_input"></li>
     </ul>
 
 </form>
@@ -16,6 +56,11 @@ include_once("classes/class.Search.php");
 <div id="tracks">
 
 </div>
+
+        </div>
+    <div class="col-sm-1 ">
+        </div>
+    </div>
 
 <script>
     $('#searchParam').on('input', function() {
@@ -25,10 +70,10 @@ include_once("classes/class.Search.php");
             $.get('results.php?type=mass&param='+searchParam, function(data){
                 var selectionString = ""
                 for(i=0; i< data.length; i++){
-                    selectionString += "<li class= 'requestThisSong' onClick='requestSong("+data[i].id+");' data-id='"+data[i].id+"'><strong>"+data[i].artist+"</strong> "+data[i].name+"</li>";
+                    selectionString += " <a href='#' class='list-group-item list-group-item-action' onClick='requestSong("+data[i].id+");' data-id='"+data[i].id+"'><strong>"+data[i].artist+"</strong> "+data[i].name+"</a>";
                 }
 
-                $("#tracks").html('<ul>'+selectionString+'</ul>');
+                $("#tracks").html('<div class="list-group">'+selectionString+'</div>');
 
 
             });
@@ -41,6 +86,7 @@ include_once("classes/class.Search.php");
         var postRequest = $.post( 'submit.php', { id: song } );
         postRequest.done(function( response ) {
             alert(response);
+
         });
     }
 
@@ -48,9 +94,8 @@ include_once("classes/class.Search.php");
 
 
 </script>
-
-
-
+</body>
+</html>
 
 
 
